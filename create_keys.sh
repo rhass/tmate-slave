66e9cdfccd597232002e0f98e318810891fa7122
Ryan Hass <ryan@invalidchecksum.net>
2016-03-16 18:51:40 -0700

    Update create_keys.sh script to improve security.
    This removes the -E flag as it would break builds, and also downgraded
    the hash algorithm from the default sha256 to md5. Additionally, this
    changes the ECDSA (NIST NSA curve) keytype to Ed25519 and sets the key
    size to 512 bits. For RSA the key size has been increased from the
    default to 4096 bits.

--- create_keys.sh
+++ create_keys.sh
@@ -3,11 +3,17 @@ gen_key() {
   keytype=$1
   ks="${keytype}_"
   key="keys/ssh_host_${ks}key"
+  if [ "$keytype" == "ed25519" ] ; then
+    bits=521
+  else
+    bits=4096
+  fi
+
   if [ ! -e "${key}" ] ; then
-    ssh-keygen -t ${keytype} -f "${key}" -N '' -E md5
+    ssh-keygen -t ${keytype} -f "${key}" -N '' -b $bits
     return $?
   fi
 }
 
 mkdir -p keys
-gen_key rsa && gen_key ecdsa || exit 1
+gen_key rsa && gen_key ed25519 || exit 1
